f2100a8d8c16e9b6841d986ec15d976e1e73be09
Willem van Bergen <willem@vanbergen.org>
2014-11-26 19:55:24 -0500

    Performance tweaks:
    - Lazy load schema.
    - Cache limits.

--- lib/active_record/validations/string_truncator.rb
+++ lib/active_record/validations/string_truncator.rb
@@ -6,16 +6,11 @@ module ActiveRecord
       module ClassMethods
         def truncate_string(field)
           column = self.columns_hash[field.to_s]
-          limit = column.limit
-
-          if column.text? && column.collation !~ /\Autf8_/
-            raise ArgumentError, "Only UTF-8 textual columns are supported."
-          end
-
           case column.type
           when :string
             lambda do
               return if self.changes[field].nil?
+              limit = StringTruncator.mysql_textual_column_limit(column)
               value = self[field].to_s
               if value.length > limit
                 self[field] = value.slice(0, limit)
@@ -25,12 +20,22 @@ module ActiveRecord
           when :text
             lambda do
               return if self.changes[field].nil?
-              value = self[field].to_s.encode('utf-8')
+              limit = StringTruncator.mysql_textual_column_limit(column)
+              value = self[field].to_s
+              value.encode!('utf-8') if value.encoding != Encoding::UTF_8
               if value.bytesize > limit
                 self[field] = value.mb_chars.limit(limit).to_s
               end
             end
           end
+        end
+      end
+
+      def self.mysql_textual_column_limit(column)
+        @mysql_textual_column_limits ||= {}
+        @mysql_textual_column_limits[column] ||= begin
+          raise ArgumentError, "Only UTF-8 textual columns are supported." unless column.text? && column.collation =~ /\Autf8_/
+          column.limit
         end
       end
     end
